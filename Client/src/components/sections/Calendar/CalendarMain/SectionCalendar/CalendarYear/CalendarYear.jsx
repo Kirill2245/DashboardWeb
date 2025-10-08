4395580a1e1a431074cea6536e826e97a5cb27b6
Kirill <publickirill2245@gmail.com>
2025-10-08 20:31:11 +0500

    Create section Calendar Year

--- Client/src/components/sections/Calendar/CalendarMain/SectionCalendar/CalendarYear/CalendarYear.jsx
+++ Client/src/components/sections/Calendar/CalendarMain/SectionCalendar/CalendarYear/CalendarYear.jsx
@@ -1,12 +1,38 @@
-
+import { useState } from 'react';
 import styles from './styles.module.css';
-
+import Calendar from 'react-calendar';
 const CalendarYear = () => {
-
+    const [value, onChange] = useState(new Date());
+    const months = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+    const fixedDate = (month) => {
+        const currentYear = new Date().getFullYear();
+        return new Date(currentYear, month, 1)
+    } 
+    const formatShortWeekday = (locale, date) => {
+        const weekdays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+        return weekdays[date.getDay()];
+    };
+    const formatMonthYear = (locale, date) => {
+        return date.toLocaleDateString('en-US', { 
+            month: 'long', 
+            year: 'numeric' 
+        });
+    };
     return(
         <div className= {styles.calendar}>
-            Y
-        </div>
+            {months.map((item,index) => (
+                <Calendar 
+                    onChange={onChange}
+                    value={value}
+                    key={index}
+                    activeStartDate={fixedDate(item)}
+                    formatShortWeekday={formatShortWeekday}
+                    formatMonthYear={formatMonthYear}
+                    prev2Label={null}  
+                    next2Label={null}  
+                />
+            ))}
+        </div>  
     );
 };
 
